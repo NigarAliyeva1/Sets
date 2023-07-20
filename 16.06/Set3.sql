af52122c0331e6d9c230f449ae650223e5676278
NigarAliyeva1 <112957859+NigarAliyeva1@users.noreply.github.com>
2023-07-20 23:58:40 +0400

    Update Set3.sql

--- 16.06/Set3.sql
+++ 16.06/Set3.sql
@@ -5,7 +5,7 @@ select department_id from employees where salary>(select avg(salary) from employ
 --2. (WHERE/HAVING Subquery) Discover departments where the maximum salary is greater than the average salary of all departments.
 select department_id from employees where salary>(select avg(salary) from employees) order by salary desc fetch first 1 row only;
 --3. (WHERE/HAVING Subquery) Find departments where the average tenure is above 5 years.
-
+select department_id from employees where extract(year from sysdate)- extract(year from hire_date)>5 group by department_id;
 --4. (WHERE/HAVING Subquery) Find departments where the number of subordinates per manager is above the company average.
 
 --5. (WHERE/HAVING Subquery) Find job titles where the maximum salary is higher than the average salary of all job titles.
